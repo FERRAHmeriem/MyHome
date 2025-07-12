b55dc8591866c6436b0bd58910d88ced002c7f33
Meriem FERRAH <149694634+FERRAHmeriem@users.noreply.github.com>
2025-07-12 19:09:40 +0100

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   server:{
+    host: '0.0.0.0',
+    port: process.env.PORT || 5173, 
+    allowedHosts: ['oulamuna.onrender.com'],
     proxy:{
       '/api':{
         target:'https://myhome-backend-anuw.onrender.com',
