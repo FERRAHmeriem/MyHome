a9a3e095ed5cd9b0f8d14afcba13d12d4711f9b7
Meriem FERRAH <149694634+FERRAHmeriem@users.noreply.github.com>
2025-07-12 19:01:47 +0100

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server:{
     proxy:{
       '/api':{
-        target:'http://localhost:3000',
+        target:'https://myhome-backend-anuw.onrender.com',
         secure: false,
       },
     },
