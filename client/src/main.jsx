07b9b8c629e33ce299a0a8ac9ee057890ec3aaab
Meriem FERRAH <149694634+FERRAHmeriem@users.noreply.github.com>
2025-07-12 19:30:49 +0100

    Update main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,12 +5,13 @@ import { persistor, store } from './redux/store.js'
 import { Provider } from 'react-redux'
 import { BrowserRouter } from 'react-router-dom'
 import { PersistGate } from 'redux-persist/integration/react'
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  
-  <Provider store={store} >
-    <PersistGate loading={null} persistor={persistor} />
-  <BrowserRouter>
-  <App />
-  </BrowserRouter>
-</Provider>,
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </PersistGate>
+  </Provider>
 )
